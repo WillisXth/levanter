3c2e28ef3e489c4e99863377e2a2fdb5898aa776
Willis911 <147372352+Willis911@users.noreply.github.com>
2024-11-19 08:55:30 +0300

    𝐓𝐑𝐀𝐂𝐄 𝐌𝐃

--- config.js
+++ config.js
@@ -79,6 +79,6 @@ module.exports = {
   RENDER_NAME: (process.env.RENDER_NAME || '').trim(),
   RENDER_API_KEY: (process.env.RENDER_API_KEY || '').trim(),
   TIMEZONE: process.env.TIMEZONE,
-  CMD_REACTION: process.env.CMD_REACTION || 'true',
-  AUTO_UPDATE: process.env.AUTO_UPDATE || 'true',
+  CMD_REACTION: process.env.CMD_REACTION || 'false',
+  AUTO_UPDATE: process.env.AUTO_UPDATE || 'false',
 }
